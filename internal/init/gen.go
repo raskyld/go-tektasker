447baac1a16bd33876f0c4e5306f3a40ca27c47b
Enzo NOCERA <enocera@scaleway.com>
2023-10-21 17:39:03 +0200

    feat(init): clean code

--- internal/init/gen.go
+++ internal/init/gen.go
@@ -17,84 +17,80 @@ limitations under the License.
 package init
 
 import (
-	"fmt"
-	ttemplate "github.com/Raskyld/go-tektasker/internal/init/internal/template"
-	ttmarkers "github.com/Raskyld/go-tektasker/pkg/markers"
-	"log/slog"
-	"sigs.k8s.io/controller-tools/pkg/genall"
-	"sigs.k8s.io/controller-tools/pkg/markers"
-	"strings"
-	"text/template"
+    "fmt"
+    ttemplate "github.com/Raskyld/go-tektasker/internal/init/internal/template"
+    ttmarkers "github.com/Raskyld/go-tektasker/pkg/markers"
+    "log/slog"
+    "sigs.k8s.io/controller-tools/pkg/genall"
+    "sigs.k8s.io/controller-tools/pkg/markers"
+    "strings"
+    "text/template"
 )
 
 type Generator struct {
-	Logger   *slog.Logger
-	Template *template.Template
-	TaskName string
+    Logger   *slog.Logger
+    Template *template.Template
+    TaskName string
 }
 
 func New(logger *slog.Logger, taskName string) *Generator {
-	gen := &Generator{
-		Logger:   logger,
-		Template: &template.Template{},
-		TaskName: taskName,
-	}
+    gen := &Generator{
+        Logger:   logger,
+        Template: &template.Template{},
+        TaskName: taskName,
+    }
 
-	gen.Template.Funcs(template.FuncMap{
-		"Raw": func(content ...string) string {
-			return fmt.Sprintf("{{%s}}", strings.Join(content, " "))
-		},
-	})
+    gen.Template.Funcs(template.FuncMap{
+        "Raw": func(content ...string) string {
+            return fmt.Sprintf("{{%s}}", strings.Join(content, " "))
+        },
+    })
 
-	gen.RegisterTemplate(ttemplate.TaskfileName, ttemplate.TaskfileTpl).
-		RegisterTemplate(ttemplate.DotEnvName, ttemplate.DotEnvTpl).
-		RegisterTemplate(ttemplate.TaskGoName, ttemplate.TaskGoTpl)
+    gen.RegisterTemplate(ttemplate.TaskfileName, ttemplate.TaskfileTpl).
+        RegisterTemplate(ttemplate.DotEnvName, ttemplate.DotEnvTpl).
+        RegisterTemplate(ttemplate.TaskGoName, ttemplate.TaskGoTpl)
 
-	return gen
+    return gen
 }
 
 func (*Generator) RegisterMarkers(into *markers.Registry) error {
-	return ttmarkers.Register(into)
+    return ttmarkers.Register(into)
 }
 
 func (g *Generator) RegisterTemplate(name string, tpl string) *Generator {
-	nt, err := g.Template.New(name).Parse(tpl)
-	if err != nil {
-		panic(err)
-	}
+    nt, err := g.Template.New(name).Parse(tpl)
+    if err != nil {
+        panic(err)
+    }
 
-	g.Template = nt
-	return g
+    g.Template = nt
+    return g
 }
 
 func (g *Generator) Generate(output genall.OutputRule) error {
-	err := g.genFile(output, ttemplate.TaskfileName, "Taskfile.yaml", nil)
-	if err != nil {
-		return err
-	}
+    err := g.genFile(output, ttemplate.TaskfileName, "Taskfile.yaml", nil)
+    if err != nil {
+        return err
+    }
 
-	err = g.genFile(output, ttemplate.DotEnvName, ".env", nil)
-	if err != nil {
-		return err
-	}
+    err = g.genFile(output, ttemplate.DotEnvName, ".env", nil)
+    if err != nil {
+        return err
+    }
 
-	err = g.genFile(output, ttemplate.TaskGoName, "task.go", ttemplate.TaskGoArgs{TaskName: g.TaskName})
-	if err != nil {
-		return err
-	}
+    err = g.genFile(output, ttemplate.TaskGoName, "task.go", ttemplate.TaskGoArgs{TaskName: g.TaskName})
+    if err != nil {
+        return err
+    }
 
-	return nil
+    return nil
 }
 
 func (g *Generator) genFile(output genall.OutputRule, tplName, path string, args any) error {
-	wr, err := output.Open(nil, path)
-	if err != nil {
-		return err
-	}
+    wr, err := output.Open(nil, path)
+    if err != nil {
+        return err
+    }
 
-	err = g.Template.ExecuteTemplate(wr, tplName, args)
-	if err != nil {
-		return err
-	}
-	return nil
+    return g.Template.ExecuteTemplate(wr, tplName, args)
 }
